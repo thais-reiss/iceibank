42a48321e26507a0762c7725fc1b8d5326e3e242
thais-reiss <thaisthatareis@gmail.com>
2026-08-31 22:06:02 -0300

    feat(java): implementa estrutura inicial em java

---
+++ agencia-java/src/main/java/com/iceibanck/agencia_java/AgenciaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.iceibanck.agencia_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaJavaApplication.class, args);
+	}
+
+}
---
+++ agencia-java/src/test/java/com/iceibanck/agencia_java/AgenciaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iceibanck.agencia_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
